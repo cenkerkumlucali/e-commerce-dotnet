fc19096430a3fbd75a13dbb808a597377b3a6e26
Cenker Kumlucalı <cenkerkumlucali0@gmail.com>
2022-10-25 22:47:24 +0300

    update Registration

--- src/Infrastructure/ECommerce.Infrastructure/ServiceRegistration.cs
+++ src/Infrastructure/ECommerce.Infrastructure/ServiceRegistration.cs
@@ -1,7 +1,9 @@
 using System.Threading.Tasks.Dataflow;
 using ECommerce.Application.Abstractions.Storage;
+using ECommerce.Application.Abstractions.Storage.Azure;
 using ECommerce.Infrastructure.Enums;
 using ECommerce.Infrastructure.Services.Storage;
+using ECommerce.Infrastructure.Services.Storage.Azure;
 using ECommerce.Infrastructure.Services.Storage.Local;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -12,30 +14,33 @@ public static class ServiceRegistration
     public static IServiceCollection AddInfrastructureServices(this IServiceCollection services)
     {
         services.AddScoped<IStorageService, StorageService>();
+        services.AddScoped<IAzureStorage, AzureStorage>();
         return services;
     }
 
-    public static IServiceCollection AddStorage<T>(this IServiceCollection services) where T: class,IStorage
+    public static IServiceCollection AddStorage<T>(this IServiceCollection services) where T : Storage, IStorage
     {
         services.AddScoped<IStorage, T>();
         return services;
     }
-    
-    public static IServiceCollection AddStorage(this IServiceCollection services,StorageType storageType)
+
+    public static IServiceCollection AddStorage(this IServiceCollection services, StorageType storageType)
     {
         switch (storageType)
         {
             case StorageType.Local:
-                services.AddScoped<IStorage,LocalStorage>();
+                services.AddScoped<IStorage, LocalStorage>();
                 break;
             case StorageType.Azure:
+                services.AddScoped<IStorage, AzureStorage>();
                 break;
             case StorageType.AWS:
                 break;
             default:
-                services.AddScoped<IStorage,LocalStorage>();
+                services.AddScoped<IStorage, LocalStorage>();
                 break;
         }
+
         return services;
     }
 }
