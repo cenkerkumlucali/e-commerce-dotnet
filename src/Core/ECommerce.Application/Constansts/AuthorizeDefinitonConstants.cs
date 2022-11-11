0d6f2be3bda0a0d33606d7e09c798080c8a8a2ce
Cenker Kumlucalı <cenkerkumlucali0@gmail.com>
2022-11-11 17:49:03 +0300

    created AuthorizeDefinitonConstants

---
+++ src/Core/ECommerce.Application/Constansts/AuthorizeDefinitonConstants.cs
@@ -0,0 +1,8 @@
+namespace ECommerce.Application.Constansts;
+
+public static class AuthorizeDefinitonConstants
+{
+    public const string Baskets = "Baskets";
+    public const string Orders = "Orders";
+    public const string Products = "Products";
+}
