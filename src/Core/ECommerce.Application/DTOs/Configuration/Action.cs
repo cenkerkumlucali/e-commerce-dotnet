f879742c91ef5c4419cabcf54943e06430062fd5
Cenker Kumlucalı <cenkerkumlucali0@gmail.com>
2022-11-11 17:50:17 +0300

    created Authorize classes

---
+++ src/Core/ECommerce.Application/DTOs/Configuration/Action.cs
@@ -0,0 +1,9 @@
+namespace ECommerce.Application.DTOs.Configuration;
+
+public class Action
+{
+    public string Code { get; set; }
+    public string ActionType { get; set; }
+    public string HttpType { get; set; }
+    public string Definition { get; set; }
+}
---
+++ src/Core/ECommerce.Application/DTOs/Configuration/Menu.cs
@@ -0,0 +1,7 @@
+namespace ECommerce.Application.DTOs.Configuration;
+
+public class Menu
+{
+    public string Name { get; set; }
+    public List<Action> Actions { get; set; } = new();
+}
