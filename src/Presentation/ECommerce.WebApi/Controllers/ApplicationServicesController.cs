e1d40b6d54ab4942fc05cf1a09228b0a9d6af68a
Cenker Kumlucalı <cenkerkumlucali0@gmail.com>
2022-11-14 17:49:38 +0300

    Created ApplicationServicesController for authorize operations

--- src/Presentation/ECommerce.WebApi/Controllers/ApplicationServicesController.cs
+++ src/Presentation/ECommerce.WebApi/Controllers/ApplicationServicesController.cs
@@ -1,10 +1,14 @@
 using ECommerce.Application.Abstractions.Services.Configurations;
+using ECommerce.Application.CustomAttributes;
+using ECommerce.Application.Enums;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ECommerce.WebApi.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Authorize(AuthenticationSchemes = "Admin")]
     public class ApplicationServicesController : ControllerBase
     {
         private readonly IApplicationService _applicationService;
@@ -14,6 +18,8 @@ namespace ECommerce.WebApi.Controllers
             _applicationService = applicationService;
         }
 
+        [HttpGet]
+        [AuthorizeDefinition(ActionType = ActionType.Reading,Menu = "Application Services",Definition = "Get Authorize Definition Endpoints")]
         public IActionResult GetAuthorizeDefinitionEndpoints()
         {
             var data = _applicationService.GetAuthorizeDefinitionEndpoints(typeof(Program));
